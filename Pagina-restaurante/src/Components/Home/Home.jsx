7171b7e2819ff3b8b292ff321d6b4dfd574fb7a7
valenti94br <valenti94br@gmail.com>
2023-05-07 23:48:18 +0200

    NavBar arregladita y con colorinchis

--- Pagina-restaurante/src/Components/Home/Home.jsx
+++ Pagina-restaurante/src/Components/Home/Home.jsx
@@ -1,16 +1,13 @@
-
 import React from 'react';
 
 function Home() {
   return (
     <div className="bg-dark text-white min-vh-100 d-flex align-items-center justify-content-center">
-      <h1>Bienvenido a nuestro restaurante</h1>
-      <p>
-        Disfruta de nuestra deliciosa comida y nuestro ambiente acogedor.
-      </p>
-      <p>
-        ¡No olvides reservar una mesa para asegurar tu lugar en nuestro restaurante!
-      </p>
+      <div className="text-center">
+        <h1>Bienvenido a nuestro restaurante</h1>
+        <p>Disfruta de nuestra deliciosa comida y nuestro ambiente acogedor.</p>
+        <p>¡No olvides reservar una mesa para asegurar tu lugar en nuestro restaurante!</p>
+      </div>
     </div>
   );
 }
--- Pagina-restaurante/src/Components/NavBar/NavBar.jsx
+++ Pagina-restaurante/src/Components/NavBar/NavBar.jsx
@@ -1,21 +1,33 @@
-// NavBar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/reserve">Reservar</Link>
-        </li>
-        <li>
-          <Link to="/carta">Carta</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'skyblue' }}>
+      <div className="container">
+        <Link className="navbar-brand text-dark" to="/">
+          Restaurante
+        </Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/" activeClassName="active">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/reserve" activeClassName="active">
+                Reservar
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/carta" activeClassName="active">
+                Carta
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
